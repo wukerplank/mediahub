d19085fd9254b218e6513dc82232ac600e8a12a2
Christoph Edthofer <wukerplank@edthofer.at>
2015-04-04 21:48:55 +0200

    Fixes the project name in the nav bar and changes the root path to the files controller.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :watch_folders
   mount Sidekiq::Web => '/sidekiq'
 
-  root 'watch_folders#index'
+  root 'media_files#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
