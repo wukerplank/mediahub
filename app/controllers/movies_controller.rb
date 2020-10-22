d9ec553140b0b1e5eef6081f8f000de921ed5c4f
Christoph Edthofer <wukerplank@edthofer.at>
2020-10-22 09:36:22 +0200

    Fix duplicates query

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,10 +10,13 @@ class MoviesController < ApplicationController
       when 'withoutid'
         @movies = @movies.where(imdb_id: nil)
       when 'duplicates'
-        @duplicates = MediaFile.connection.select_all("SELECT imdb_id, COUNT(*) AS duplicates FROM media_files
-        WHERE imdb_id IS NOT NULL AND imdb_id <> \"\"
-        GROUP BY imdb_id
-        HAVING duplicates > 1")
+        @duplicates = MediaFile.connection.select_all("
+          SELECT imdb_id FROM (
+            SELECT imdb_id, COUNT(*) AS duplicates FROM media_files
+            WHERE imdb_id IS NOT NULL AND imdb_id <> ''
+            GROUP BY imdb_id
+          ) AS subquery
+          WHERE duplicates > 1")
 
         @movies = @movies.where(imdb_id: @duplicates.map{ |d| d['imdb_id'] } )
       end
